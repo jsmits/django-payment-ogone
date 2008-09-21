e2b4051c3a868f4d0c5dd19d4c7c292b9b8f3d1d
Sander Smits <jsmits@iMac.lan>
2008-09-21 15:40:32 +0200

    changed defaults for PSPID, SHA1_PRE_SECRET and SHA1_POST_SECRET

--- ogone/conf/settings.py
+++ ogone/conf/settings.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-PSPID = getattr(settings, "OGONE_PSPID", "dummy_pspid")
+PSPID = getattr(settings, "OGONE_PSPID")
 TEST_URL = getattr(settings, "OGONE_TEST_URL", 
     "https://secure.ogone.com/ncol/test/orderstandard.asp")
 PROD_URL = getattr(settings, "OGONE_PROD_URL", 
@@ -11,5 +11,5 @@ LANGUAGE = 'nl_NL'
 # LANGUAGE = 'en_US'
 CURRENCY = 'EUR'
 
-SHA1_PRE_SECRET = getattr(settings, "OGONE_SHA1_PRE_SECRET", 'dummy_pre_secret')
-SHA1_POST_SECRET = getattr(settings, "OGONE_SHA1_POST_SECRET", 'dummy_post_secret')
+SHA1_PRE_SECRET = getattr(settings, "OGONE_SHA1_PRE_SECRET", '')
+SHA1_POST_SECRET = getattr(settings, "OGONE_SHA1_POST_SECRET", '')
