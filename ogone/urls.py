78ac263bd40318b58590f53616fa5832c11dcbe9
Sander Smits <jsmits@iMac.lan>
2008-10-05 14:16:20 +0200

    changed declined and exception url to order_status_update view

--- ogone/urls.py
+++ ogone/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('ogone.views',
     url(r'^status/$', 'order_status_update'),
     
     url(r'^accepted/', 'order_status_update'),
-    url(r'^declined/', 'declined'),
-    url(r'^exception/', 'exception'),
+    url(r'^declined/', 'order_status_update'),
+    url(r'^exception/', 'order_status_update'),
     url(r'^cancelled/', 'order_status_update'),
     
     url(r'^to_ogone/$', 'to_ogone'),
