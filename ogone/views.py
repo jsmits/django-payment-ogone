fed86aa305ee4063e7a2ea4a1ebba82a4154d226
Sander Smits <jsmits@iMac.lan>
2008-10-12 11:55:10 +0200

    added RequestContext to status responses

--- ogone/views.py
+++ ogone/views.py
@@ -110,13 +110,16 @@ def order_status_update(request):
             signals.ogone_payment_accepted.send(sender=OrderStatus, order_id=order_id, 
                 amount=Decimal(amount) * 100, currency=currency)
             # return the appropiate response
-            return render_to_response('ogone/payment_accepted.html')
+            return render_to_response('ogone/payment_accepted.html',
+                context_instance=RequestContext(request))
         # cancelled
         elif status == u'1':
-            return render_to_response('ogone/payment_cancelled.html')
+            return render_to_response('ogone/payment_cancelled.html', 
+                context_instance=RequestContext(request))
         elif int(status) in [0,2,4,41,5,51,52,59,6,61,62,63,7,71,72,73,74,75,
             8,81,82,83,84,85,91,92,93,94,95,97,98,99]:
-            return render_to_response('ogone/payment_not_processed.html')
+            return render_to_response('ogone/payment_not_processed.html',
+                context_instance=RequestContext(request))
         else:
             # mail_admins
             subject = 'Error (%s IP): %s' % ((request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS and 'internal' or 'EXTERNAL'), request.path)
@@ -126,8 +129,10 @@ def order_status_update(request):
                 request_repr = "Request repr() unavailable"
             message = "Unknown ogone status code: %s\n\n%s" % (status, request_repr)
             mail_admins(subject, message, fail_silently=True)
-            return render_to_response('ogone/payment_not_processed.html')
+            return render_to_response('ogone/payment_not_processed.html',
+                context_instance=RequestContext(request))
     else:
-        return render_to_response('ogone/payment_not_processed.html')
+        return render_to_response('ogone/payment_not_processed.html',
+            context_instance=RequestContext(request))
     
         
